26e0b8cc7958ab316d915421b912f2c84c8b47b6
Bri Chapman <brichapman@wirelessprvnat-172-17-13-131.near.illinois.edu>
2014-09-05 12:50:44 -0500

    printing processes works

--- Processes/BCCViewController.m
+++ Processes/BCCViewController.m
@@ -7,7 +7,12 @@
 //
 
 #import "BCCViewController.h"
-
+#import <mach/mach.h>
+#import <sys/utsname.h>
+#import <sys/resource.h>
+#import <sys/syslog.h>
+#import <sys/types.h>
+#import <sys/sysctl.h>
 @interface BCCViewController ()
 
 @end
@@ -18,6 +23,55 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0};
+    size_t miblen = 4;
+    
+    size_t size;
+    int st = sysctl(mib, miblen, NULL, &size, NULL, 0);
+    
+    struct kinfo_proc * process = NULL;
+    struct kinfo_proc * newprocess = NULL;
+    
+    do {
+        
+        size += size / 10;
+        newprocess = realloc(process, size);
+        
+        if (!newprocess){
+            if (process){
+                free(process);
+            }
+        }
+        
+        process = newprocess;
+        st = sysctl(mib, miblen, process, &size, NULL, 0);
+        
+    } while (st == -1 && errno == ENOMEM);
+    
+    if (st == 0){
+        if (size % sizeof(struct kinfo_proc) == 0){
+            int nprocess = size / sizeof(struct kinfo_proc);
+            
+            if (nprocess){
+                
+                NSMutableArray * array = [[NSMutableArray alloc] init];
+                
+                for (int i = nprocess - 1; i >= 0; i--){
+                    
+                    NSString * processID = [[NSString alloc] initWithFormat:@"%d", process[i].kp_proc.p_pid];
+                    NSString * processName = [[NSString alloc] initWithFormat:@"%s", process[i].kp_proc.p_comm];
+                    
+                    NSDictionary * dict = [[NSDictionary alloc] initWithObjects:[NSArray arrayWithObjects:processID, processName, nil]
+                                                                        forKeys:[NSArray arrayWithObjects:@"ProcessID", @"ProcessName", nil]];
+                    [array addObject:dict];
+                }
+                
+                free(process);
+                NSLog(@"System pid: %@", array);
+            }
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
